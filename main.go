c2dd0a358ba709c68c28632a0940f9e242075510
Gavin Stark <gstark31897@gmail.com>
2015-03-02 20:34:42 -0600

    added inital web code

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
-func main()
-{
-  fmt.Println("Hello World")
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
