bf940c6d3bd026655af3b02565872b923c035695
gstark31897 <gstark31897@gmail.com>
2015-03-03 14:41:04 -0500

    back to the begining

--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net/http"
-	"io/ioutil"
+    "fmt"
+    "net/http"
 )
-
-var file_path string
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
-
-	files, _ := ioutil.ReadDir(file_path)
-  	for _, file := range files {
-    		fmt.Println(file)
-  	}
 }
 
 func main() {
-	file_path := os.Args[1]
-	fmt.Println("file-server-path=", file_path)
-
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
