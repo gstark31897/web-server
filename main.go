2ffa704b5e1686cdef5e20b1567c03adcda81f4b
Gavin Stark <gstark31897@gmail.com>
2015-03-02 20:41:03 -0600

    testing command line args

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "os"
     "net/http"
 )
 
@@ -10,6 +11,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    argsWithProg := os.Args
+    argsWithoutProg := os.Args[1:]
+    arg := os.Args[3]
+    fmt.Println(argsWithProg)
+    fmt.Println(argsWithoutProg)
+    fmt.Println(arg)
+
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
