4feae2dbedeb7e2134df6e07cf011692cf6da7b7
Gavin Stark <gstark31897@gmail.com>
2015-03-02 20:55:02 -0600

    testing command line args

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
 
-import (
-    "fmt"
-    "os"
-    "net/http"
-    "io/ioutil"
+import
+(
+	"fmt"
+	"os"
+	"net/http"
+	"io/ioutil"
 )
 
 var file_path string
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World")
+func handler(w http.ResponseWriter, r *http.Request)
+{
+	fmt.Fprintf(w, "Hello World")
 
-    files, _ := ioutil.ReadDir(file_path)
-    for _, f := range files {
-        fmt.Println(f.Name())
-    }
+	files, _ := ioutil.ReadDir(file_path)
+	for _, file := range files
+	{
+    fmt.Println(file)
+	}
 }
 
-func main() {
-    file_path := os.Args[1]
-    fmt.Println("file-server-path=", file_path)
+func main()
+{
+	file_path := os.Args[1]
+	fmt.Println("file-server-path=", file_path)
 
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
