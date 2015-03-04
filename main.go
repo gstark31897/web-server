cc9f8dd8519b8d50ec3b854ef22d390171cf7f23
gstark31897 <gstark31897@gmail.com>
2015-03-03 23:12:06 -0500

    added directory listing

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "io/ioutil"
 )
 
+var server_file_path = "/home/octalus/file-server"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+    fmt.Fprintf(w, "Hello World")
+    fmt.Println("Serving Request")
 }
 
 func main() {
+    files, _ := ioutil.ReadDir(server_file_path)
+    for _, f := range files {
+            fmt.Println(f.Name())
+    }
+
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
